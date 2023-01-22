f45e0c729b3197b7052c868d2d293925e9dfcfde
Kirill Karavaev <coffee.2016@yandex.ru>
2023-01-22 18:39:05 +0300

    Работа в классе

---
+++ Zadacha49/Program.cs
@@ -0,0 +1,87 @@
+﻿//  Матрица случайных чисел
+// Console.Clear();
+
+// int [,] matrix = new int[4,4];
+// FillArray(matrix);
+// PrintArray(matrix);
+// Console.WriteLine();
+// Console.WriteLine("Задача 49.");
+// FillArraySqr(matrix);
+// PrintArray(matrix);
+// Console.WriteLine();
+// Console.WriteLine("Задача 51.");
+// FillArrayDiag(matrix);
+
+// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FillArraySqr(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             if (i % 2 == 0 && j % 2 == 0) matr[i,j] = matr[i,j] * matr[i,j];
+//         }
+//     }
+// }
+
+// void FillArrayDiag(int[,] matr)
+// {
+//     int count = 0;
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             if (i == j) count += matr[i,j];
+//         }
+//     }
+
+//     Console.WriteLine($"Сумма чисел диагонали: {count}");
+// }
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for(int i = 0; i<matr.GetLength(0); i++)
+    {
+        for (int j = 0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void FillArraySqr(int[,] matr)
+{
+    for(int i = 0; i<matr.GetLength(0); i++)
+    {
+        for (int j = 0; j<matr.GetLength(1); j++)
+        {
+            if(i % 2 ==0 && j % 2 ==0) matr[i,j] = matr[i,j]*matr[i,j];
+        }
+    }
+}
+int[,] Matrix = new int[4,4];
+FillArray(Matrix);
+PrintArray(Matrix);
+Console.WriteLine();
+FillArraySqr(Matrix);
+PrintArray(Matrix);
---
+++ Zadacha51/Program.cs
@@ -0,0 +1,40 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for(int i = 0; i<matr.GetLength(0); i++)
+    {
+        for (int j = 0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void MainDiagSum(int[,] matr)
+{
+    int sum = 0;
+    for(int i = 0; i<matr.GetLength(0); i++)
+    {
+        for(int j = 0; j<matr.GetLength(1); j++)
+        {
+            if(i==j) sum = sum + matr[i,j];
+        }
+    }
+    Console.WriteLine($"Сумма элементов главной диагонали равна {sum}");
+}
+
+int[,] matrix = new int [5,5];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+MainDiagSum(matrix);
