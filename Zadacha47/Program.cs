785a7cd88ef7ca494087e9ea492d1a44cae80419
Kirill Karavaev <coffee.2016@yandex.ru>
2023-01-22 18:40:27 +0300

    Добавлено решение задачи 47 из дз, и попытка решения задачи 50

---
+++ Zadacha47/Program.cs
@@ -0,0 +1,26 @@
+﻿//Вывод на экран матрицы со случайными вещественными числами
+void PrintArray(double[,] matr)
+{
+    for (var i = 0; i < matr.GetLength(0); i++)
+    {
+        for (var j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(double[,] matr)
+{
+    for(var i = 0; i<matr.GetLength(0); i++)
+    {
+        for (var j = 0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().NextDouble() * new Random().Next(-9,10);
+        }
+    }
+}
+
+double[,] matrix = new double [3,4];
+FillArray(matrix);
+PrintArray(matrix);
---
+++ Zadacha50/Program.cs
@@ -0,0 +1,52 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for(int i = 0; i<matr.GetLength(0); i++)
+    {
+        for (int j = 0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = i+j;
+        }
+    }
+}
+void FindNum (int[,] matr, int X)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (X==i*j) 
+            {
+                Console.WriteLine(matr[i,j]);
+                break;
+            }
+            else 
+            {
+            Console.WriteLine("Такого числа в массиве нет");
+            }
+        }
+        
+    }
+}
+
+Console.WriteLine("Введите количество строк массива");
+int Lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int Columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите искомую позицию");
+int Pos = Convert.ToInt32(Console.ReadLine());
+
+int[,] Matrix = new int [Lines, Columns];
+FillArray(Matrix);
+PrintArray(Matrix);
+FindNum(Matrix, Pos);
