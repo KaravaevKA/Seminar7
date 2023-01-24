3fe2192315aa73d79221770219f55d882a36107b
Kirill Karavaev <coffee.2016@yandex.ru>
2023-01-24 21:02:07 +0300

    Добавлено решение задачи 52 из дз

---
+++ Zadacha52/Program.cs
@@ -0,0 +1,46 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for(int i = 0; i<matr.GetLength(0); i++)
+    {
+        for (int j = 0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1, 100);
+        }
+    }
+}
+void AverageSum (int[,] matr)
+{
+   
+   double SumAvg = 0;
+   for (int j = 0; j < matr.GetLength(1); j++)
+   {
+    double sum = 0;
+    for (int i= 0; i < matr.GetLength(0); i++)
+    {
+        sum += matr[i,j];
+    }
+    SumAvg = sum / matr.GetLength(1);
+    Console.WriteLine($"Среднее значение {j+1} столбца равно: {SumAvg}");
+   }
+}
+
+Console.WriteLine("Введите количество столбцов");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество рядов");
+int row = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[row,col];
+
+FillArray(array);
+PrintArray(array);
+AverageSum(array);
