b860a77b8ad9fe6220b8eb0b43de400a06eec42e
Kirill Karavaev <coffee.2016@yandex.ru>
2023-01-26 18:39:15 +0300

    Добавлено решение задачи 50 из дз

--- Zadacha50/Program.cs
+++ Zadacha50/Program.cs
@@ -19,34 +19,29 @@ void FillArray(int[,] matr)
         }
     }
 }
-void FindNum (int[,] matr, int X)
+void FindNum (int[,] matr, int x, int y)
 {
     for(int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            if (X==i*j) 
-            {
-                Console.WriteLine(matr[i,j]);
-                break;
-            }
-            else 
-            {
-            Console.WriteLine("Такого числа в массиве нет");
-            }
+            if (x == i + 1 && y == j + 1) Console.WriteLine(matr[i,j]); 
+            else Console.WriteLine("Такого числа нет");
+            
         }
-        
     }
 }
 
 Console.WriteLine("Введите количество строк массива");
-int Lines = Convert.ToInt32(Console.ReadLine());
+int lines = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов массива");
-int Columns = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите искомую позицию");
-int Pos = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите строку искомой позиции");
+int  x1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ряд искомой позиции");
+int x2 = Convert.ToInt32(Console.ReadLine());
 
-int[,] Matrix = new int [Lines, Columns];
+int[,] Matrix = new int [lines, columns];
 FillArray(Matrix);
 PrintArray(Matrix);
-FindNum(Matrix, Pos);
+FindNum(Matrix, x1, x2);
